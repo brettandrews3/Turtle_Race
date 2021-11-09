53f1613a0dd4139b05e0cc5914aabdb4a5df1bdc
Brett Andrews <brettandrews3@gmail.com>
2021-11-09 16:20:20 -0600

    Finished the race() function and set it to run after setup(). Docstring added to race(). Widened the gaps between turtles for better visibility. Program complete. Next: confirm it's setup as a module to import to go with my Game_Choices program I'm working on.

--- turtle_race.py
+++ turtle_race.py
@@ -22,8 +22,8 @@ def setup():
     screen.setup(1290, 720)
     screen.bgpic('pavement.gif')
     
-    turtle_color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-    turtle_ycor = [60, 40, 20, 0, -20, -40, -60]
+    turtle_color = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet']
+    turtle_ycor = [120, 80, 40, 0, -40, -80, -120]
     turtle_shape = ['turtle', 'arrow', 'square', 'circle', 'square', 'arrow', 'turtle']
     
     for i in range(0, len(turtle_ycor)):
@@ -36,7 +36,12 @@ def setup():
         turtles.append(new_turtle)
 
 def race():
-    """Here you go, race fans: the explanation on how this sucker works!
+    """race() drives the action of the program. It imports turtles as a global variable
+        and sets the win condition, 'winner', to False. The 'while not winner' loop cycles
+        through the turtles and moves each one forward 0-2 pixels per pass. After each
+        movement, the while loop checks the turtle's current x-coordinate (xcor). If that
+        turtle's xcor is >= the finishline value, the win condition is flipped to True
+        and the print statement announces the race winner.
     """
     global turtles
     winner = False
@@ -44,10 +49,15 @@ def race():
     
     while not winner:
         for current_turtle in turtles:
-            current_turtle.forward(randint(0,2))
-
-
-
+            move = random.randint(0,2)
+            current_turtle.forward(move)
+            
+            xcor = current_turtle.xcor()
+            if xcor >= finishline:
+                winner = True
+                winner_color = current_turtle.color()
+                print(winner_color[0], 'wins the race!')
 
 setup()
+race()
 turtle.mainloop()
